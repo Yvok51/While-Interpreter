b9d60c223661f4e8116262d28d2d7ae5c5f6602e
tichymi5 <michal.tichy.y@email.cz>
2022-05-25 22:51:38 +0200

    Formatting

--- app/Evaluation.hs
+++ app/Evaluation.hs
@@ -34,21 +34,19 @@ handleUnbound = NumberVal 0
 newtype Interpreter a = Inter { runInterpreter :: Environment -> Either ErrorMsg (a, Environment) }
 
 instance Show a => Show (Interpreter a) where
-    show (Inter f) = show (f []) -- TODO: maybe a better option is possible?
+    show (Inter f) = show (f []) -- TODO: better option possible?
 
 instance Functor Interpreter where
-    fmap f inter = Inter $ \env ->
-        case runInterpreter inter env of 
-            Right (a, newEnv) -> Right (f a, newEnv)
-            Left msg -> Left msg 
+    fmap f inter = Inter $ \env -> case runInterpreter inter env of
+        Right (a, newEnv) -> Right (f a, newEnv)
+        Left  msg         -> Left msg
 
 instance Monad Interpreter where
     return a = Inter $ \env -> Right (a, env)
 
-    (>>=) inter f = Inter $ \env ->
-        case runInterpreter inter env of
-            Right (a, newEnv) -> runInterpreter (f a) newEnv 
-            Left err -> Left err
+    (>>=) inter f = Inter $ \env -> case runInterpreter inter env of
+        Right (a, newEnv) -> runInterpreter (f a) newEnv
+        Left  err         -> Left err
 
 instance Applicative Interpreter where
     pure = return
@@ -69,25 +67,27 @@ set env = Inter $ const $ Right ((), env)
 
 findVar :: Environment -> Identifier -> Maybe Value
 findVar [] _ = Nothing
-findVar ((key, val) : xs) ident
-    | ident == key = Just val
-    | otherwise = findVar xs ident
+findVar ((key, val) : xs) ident | ident == key = Just val
+                                | otherwise    = findVar xs ident
 
 insertVar :: Environment -> Identifier -> Value -> Environment
 insertVar [] ident newVal = [(ident, newVal)]
 insertVar ((key, val) : xs) ident newVal
     | key == ident = (key, newVal) : xs
-    | otherwise = (key, val) : insertVar xs ident newVal
+    | otherwise    = (key, val) : insertVar xs ident newVal
 
 readVariable :: Identifier -> Interpreter Value
-readVariable ident = Inter $ \env -> 
-    case findVar env ident of 
-        Nothing -> Left ("Variable " ++ ident ++ " is not defined. Currently defined variables: " ++ show env)
-        Just val -> Right (val, env)
+readVariable ident = Inter $ \env -> case findVar env ident of
+    Nothing -> Left
+        (  "Variable "
+        ++ ident
+        ++ " is not defined. Currently defined variables: "
+        ++ show env
+        )
+    Just val -> Right (val, env)
 
 writeVariable :: Identifier -> Value -> Interpreter ()
-writeVariable ident val = Inter $ \env ->
-    Right ((), insertVar env ident val)
+writeVariable ident val = Inter $ \env -> Right ((), insertVar env ident val)
 
 eval :: Expr -> Interpreter Value
 eval (Bool a) = do
@@ -112,8 +112,8 @@ eval (Plus a b) = do
 eval (Var ident) = readVariable ident
 
 guardMessage :: String -> String -> ErrorMsg
-guardMessage expected encountered
-    = "Expected '" <> expected <> "', but encountered '" <> encountered <> "'"
+guardMessage expected encountered =
+    "Expected '" <> expected <> "', but encountered '" <> encountered <> "'"
 
 {-
 guardAnyValue :: Maybe Value -> Interpreter Value
@@ -135,13 +135,13 @@ exec (Assignment ident expr) = do
     writeVariable ident val
 exec (IfThen expr comTrue comFalse) = do
     val <- eval expr
-    case val of 
+    case val of
         BoolVal True -> exec comTrue
         BoolVal False -> exec comFalse
         _ -> Inter $ const $ Left "If statement requires a boolean value"
 exec (While expr com) = do
     val <- eval expr
-    case val of 
+    case val of
         BoolVal True -> do
             exec com
             exec (While expr com)
@@ -151,7 +151,7 @@ exec (Seq comFirst comSecond) = do
     exec comFirst
     exec comSecond
 
--- Test cases
+-- examples
 -- >>> exec (Assignment "L" (Plus (Number 2) (Number 2)))
 
 -- >>> exec (Seq (Assignment "L" (Plus (Number 2) (Number 2))) (Assignment "L" (Plus (Var "L") (Number 1))))
@@ -174,8 +174,6 @@ Else
 L := L + (-1)
 -}
 -- >>> exec (Seq (Assignment "L" (Number 4)) (Seq (IfThen (Not (Equals (Var "L") (Number 4))) (Assignment "L" (Plus (Var "L") (Number 1))) (Assignment "L" (Plus (Var "L") (Number 2)))) (Assignment "L" (Plus (Var "L") (Number (-1))))))
--- Right ((),[("L",NumberVal 5)])
---
 
 {-
 I := 1;
@@ -185,6 +183,4 @@ While !(Cycles = 0) Then
     I := I + 2
 -}
 -- >>> exec (Seq (Assignment "I" (Number 1)) (Seq (Assignment "Cycles" (Number 3)) (While (Not (Equals (Var "Cycles") (Number 0))) (Seq (Assignment "Cycles" (Plus (Var "Cycles") (Number (-1)))) (Assignment "I" (Plus (Var "I") (Number 2)))))))
--- Right ((),[("I",NumberVal 7),("Cycles",NumberVal 0)])
---
 

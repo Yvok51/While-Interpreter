ed93d788992fb5c40d2a4b5fe06ce7cb04f6cce7
tichymi5 <michal.tichy.y@email.cz>
2022-05-27 14:34:35 +0200

    Added parsers for expressions and commands

--- app/Parser.hs
+++ app/Parser.hs
@@ -3,16 +3,35 @@
 module Parser where
 
 import           Data.Char                      ( isDigit
+                                                , isLetter
                                                 , isSpace
+                                                )
+
+import           Evaluation                     ( Com
+                                                  ( Assignment
+                                                  , IfThen
+                                                  , Seq
+                                                  , While
+                                                  )
+                                                , Expr
+                                                  ( And
+                                                  , BinaryOp
+                                                  , Bool
+                                                  , Equals
+                                                  , Not
+                                                  , Number
+                                                  , Var
+                                                  )
+                                                , Identifier
+                                                , Op(Add)
                                                 )
 
 newtype State s a = State {runState :: s -> (s, a)}
 
 instance Functor (State s) where
   -- fmap :: (a -> b) -> State s a -> State s b
-  fmap f stavovyVypocet = State $ \s ->
-    let (s', a) = runState stavovyVypocet s
-    in  (s', f a)
+  fmap f stavovyVypocet =
+    State $ \s -> let (s', a) = runState stavovyVypocet s in (s', f a)
 
 instance Monad (State s) where
   -- return :: a -> State s a
@@ -38,6 +57,32 @@ get = State $ \s -> (s, s)
 set :: s -> State s ()
 set s = State $ const (s, ())
 
+newtype Parser a = Parser {runParser :: State String (Either ParseError a)}
+
+instance Functor Parser where
+  fmap f (Parser p) = Parser $ do
+    result <- p
+    case result of
+      Left  err -> return $ Left err
+      Right a   -> return $ Right $ f a
+
+instance Monad Parser where
+  return x = Parser $ do
+    return $ Right x
+
+  (>>=) (Parser p) f = Parser $ do
+    result <- p
+    case result of
+      Right x   -> runParser (f x)
+      Left  err -> return $ Left err
+
+instance Applicative Parser where
+  pure = return
+  mf <*> mx = do
+    f <- mf
+    x <- mx
+    return $ f x
+
 data ParseError = ParseError
   { errorExpected :: String
   , errorFound    :: String
@@ -48,8 +93,10 @@ instance Show ParseError where
   show err =
     "expected: " <> errorExpected err <> ", but found: " <> errorFound err
 
-newtype Parser a = Parser {runParser :: State String (Either ParseError a)}
+parseError :: String -> String -> Parser a
+parseError expected found = Parser $ return $ Left $ ParseError expected found
 
+-- Parse an end-of-file
 parseEof :: Parser ()
 parseEof = Parser $ do
   input <- get
@@ -57,69 +104,226 @@ parseEof = Parser $ do
     []      -> return $ Right ()
     (c : _) -> return $ Left $ ParseError "end of file" [c]
 
+-- Parse any character
 parseAny :: Parser Char
 parseAny = Parser $ do
   input <- get
   case input of
-      [] -> return $ Left expectedCharError
-      (c : xs) -> do
-                  set xs
-                  return $ Right c
+    []       -> return $ Left (ParseError "any character" "end of file")
+    (c : xs) -> do
+      set xs
+      return $ Right c
 
-expectedCharError :: ParseError
-expectedCharError = ParseError "any character" "end of file"
-
-mapParser :: (a -> b) -> Parser a -> Parser b
-mapParser f (Parser p) = Parser $ do
-  result <- p
-  case result of
-      Left err -> return $ Left err
-      Right a -> return $ Right $ f a
-
-instance Functor Parser where
-  fmap = mapParser
-
-returnParser :: a -> Parser a
-returnParser x = Parser $ do
-  return $ Right x
-
-andThenParser :: Parser a -> (a -> Parser b) -> Parser b
-andThenParser (Parser p) f = Parser $ do
-  result <- p
-  case result of
-    Right x   -> runParser (f x)
-    Left  err -> return $ Left err
-
-instance Monad Parser where
-  return = returnParser
-  (>>=)  = andThenParser
-
-instance Applicative Parser where
-  pure = return
-  mf <*> mx = do
-    f <- mf
-    x <- mx
-    return $ f x
-
+-- Combine two parsers together
+-- try the first parser, if it doesn't work try the second and return result
 (<|>) :: Parser a -> Parser a -> Parser a
 (Parser p1) <|> (Parser p2) = Parser $ do
-  backup <- get
+  backup   <- get
   resultP1 <- p1
   case resultP1 of
-      Right a -> return $ Right a
-      Left _ -> do
-          set backup
-          p2
+    Right a -> return $ Right a
+    Left  _ -> do
+      set backup
+      p2
 
-parseError :: String -> String -> Parser a
-parseError expected found = Parser $ return $ Left $ ParseError expected found
-
+-- Get a description of what we are parsing and a predicate for what we want to parse
 satisfy :: String -> (Char -> Bool) -> Parser Char
 satisfy description p = do
   c <- parseAny
-  if p c then return c
-         else parseError description [c]
+  if p c then return c else parseError description [c]
 
+-- Build a parser for a given character
+char :: Char -> Parser Char
+char c = satisfy [c] (== c)
+
+-- Parse whitespace
+space :: Parser Char
+space = satisfy "space" isSpace
+
+-- Build a parser for a digit
+digit :: Parser Char
+digit = satisfy "digit" isDigit
+
+-- Try to parse with a given parser as many times as possible
+many :: Parser a -> Parser [a]
+many p = many1 p <|> return []
+
+-- Try to parse with given parser as many times as possible but at least once
+many1 :: Parser a -> Parser [a]
+many1 p = do
+  first <- p
+  rest  <- many p
+  return (first : rest)
+
+-- Build parser for a given string
+string :: String -> Parser String
+string = mapM char
+
+-- Build a parser for continous whitespace
+spaces :: Parser String
+spaces = many space
+
+-- Build a parser for a given string and as much whitespace after it
+symbol :: String -> Parser String
+symbol s = do
+  result <- string s
+  _      <- spaces
+  return result
+
+-- Parse between what the right and left parsers parse
+between :: Parser a -> Parser c -> Parser b -> Parser b
+between left right p = do
+  _      <- left
+  result <- p
+  _      <- right
+  return result
+
+betweenParenthesis :: Parser a -> Parser a
+betweenParenthesis = between (symbol "(") (symbol ")")
+
+-- Build a parser for a non-negative integer
+parseNonNegativeInt :: Parser Int
+parseNonNegativeInt = do
+  digits <- many1 digit
+  _      <- spaces
+  return $ read digits
+
+-- Build a parser for an integer
+parseInteger :: Parser Int
+parseInteger = parseNonNegativeInt <|> parseNegativeInt
+ where
+    -- Build a parser for a negative integer
+  parseNegativeInt :: Parser Int
+  parseNegativeInt = do
+    _      <- symbol "-"
+    digits <- many1 digit
+    _      <- spaces
+    return $ -1 * read digits
+
+-- combine a list of parsers together using `<|>`
+choice :: String -> [Parser a] -> Parser a
+choice desc = foldr (<|>) noMatch where noMatch = parseError desc "no match"
+
+-- Build a parser for a boolean value
+parseBool :: Parser Bool
+parseBool = trueParser <|> falseParser
+ where
+  trueParser :: Parser Bool
+  trueParser = do
+    _ <- symbol "true"
+    return True
+
+  falseParser :: Parser Bool
+  falseParser = do
+    _ <- symbol "false"
+    return False
+
+-- Build a parser for identifiers - string of letters and underscores
+parseIdentifier :: Parser Identifier
+parseIdentifier = do
+  result <- many1 $ satisfy "identifier" (\c -> isLetter c || c == '_')
+  _      <- spaces
+  return result
+
+parseArithExpression :: Parser Expr
+parseArithExpression = do
+  _ <- spaces
+  choice
+    "Arithmetic expression"
+    [ identifierParser
+    , integerParser
+    , additionParser
+    , betweenParenthesis parseArithExpression -- TODO: maybe not ideal?
+    ]
+ where
+  identifierParser :: Parser Expr
+  identifierParser = do
+    Var <$> parseIdentifier
+
+  integerParser :: Parser Expr
+  integerParser = do
+    Number <$> parseInteger
+
+  additionParser :: Parser Expr
+  additionParser = do
+    lhs <- parseArithExpression
+    _   <- symbol "+"
+    rhs <- parseArithExpression
+    return $ BinaryOp Add lhs rhs
+
+parseBoolExpression :: Parser Expr
+parseBoolExpression = do
+  _ <- spaces
+  choice
+    "Boolean expression"
+    [ booleanParser
+    , equalityParser
+    , conjunctionParser
+    , negationParser
+    , betweenParenthesis parseBoolExpression
+    ]
+ where
+  booleanParser = do
+    Bool <$> parseBool
+
+  equalityParser = do
+    lhs <- parseArithExpression
+    _   <- symbol "="
+    rhs <- parseArithExpression
+    return $ Equals lhs rhs
+
+  negationParser = do
+    _    <- symbol "!"
+    expr <- parseBoolExpression
+    return $ Not expr
+
+  conjunctionParser = do
+    lhs <- parseBoolExpression
+    _   <- symbol "&"
+    rhs <- parseBoolExpression
+    return $ And lhs rhs
+
+parseCommand :: Parser Com
+parseCommand = do
+  _ <- spaces
+  choice "Command statement"
+         [ assignmentParser
+         , ifThenParser
+         , sequenceParser
+         , whileParser
+         ]
+ where
+  assignmentParser = do
+    ident <- parseIdentifier
+    _     <- symbol ":="
+    expr  <- parseArithExpression
+    return $ Assignment ident expr
+
+  ifThenParser = do
+    _            <- symbol "if"
+    p            <- parseBoolExpression
+    _            <- symbol "then"
+    trueCommand  <- parseCommand
+    _            <- symbol "else"
+    falseCommand <- parseCommand
+    _            <- symbol "fi"
+    return $ IfThen p trueCommand falseCommand
+
+  sequenceParser = do
+    firstCommand  <- parseCommand
+    _             <- symbol ";"
+    secondCommand <- parseCommand
+    return $ Seq firstCommand secondCommand
+
+  whileParser = do
+    _       <- symbol "while"
+    p       <- parseBoolExpression
+    _       <- symbol "do"
+    command <- parseCommand
+    _       <- symbol "od"
+    return $ While p command
+
+-- Helper function to run the parser on the given string
 run :: Parser a -> String -> Either ParseError a
 run p s = snd $ runState (runParser go) s
  where
@@ -127,54 +331,3 @@ run p s = snd $ runState (runParser go) s
     result <- p
     parseEof
     return result
-
-char :: Char -> Parser Char
-char c = satisfy [c] (== c)
-
-space :: Parser Char
-space = satisfy "space" isSpace
-
-digit :: Parser Char
-digit = satisfy "digit" isDigit
-
-many :: Parser a -> Parser [a]
-many p = many1 p <|> return []
-
-many1 :: Parser a -> Parser [a]
-many1 p = do
-  first <- p
-  rest  <- many p
-  return (first : rest)
-
-string :: String -> Parser String
-string = mapM char
-
-number :: Parser Int
-number = do
-  digits <- many1 digit
-  return $ read digits
-
-spaces :: Parser String
-spaces = many space
-
-symbol :: String -> Parser String
-symbol s = do
-  result <- string s
-  _      <- spaces
-  return result
-
-choice :: String -> [Parser a] -> Parser a
-choice desc = foldr (<|>) noMatch where noMatch = parseError desc "no match"
-
-parseBool :: Parser Bool
-parseBool = trueParser <|> falseParser
-    where
-        trueParser :: Parser Bool
-        trueParser = do
-            _ <- symbol "true"
-            return True
-
-        falseParser :: Parser Bool
-        falseParser = do
-            _ <- symbol "false"
-            return False

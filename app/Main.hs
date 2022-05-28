d67c12974e14530f8520d33293dd5d45bd0bfe51
tichymi5 <michal.tichy.y@email.cz>
2022-05-28 13:51:18 +0200

    Added a simple main function to parse a file from the command line

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,31 @@
 module Main where
 
+import           System.Environment             ( getArgs )
+import           System.IO                      ( hFlush
+                                                , stdout
+                                                )
+
+import           Evaluation                     ( execProgram )
+import           Parser                         ( parseProgram )
+
+read' :: IO String
+read' = do
+    putStr "WHILE> "
+    hFlush stdout
+    getLine
+
+eval' :: String -> String
+eval' input = case parseProgram input of
+    Left  err -> show err
+    Right com -> show $ execProgram com
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    args <- getArgs
+    case args of
+        [file] -> do
+            contents <- readFile file
+            putStrLn $ eval' contents
+        _ -> do
+            putStrLn "Wrong number of arguments"
+

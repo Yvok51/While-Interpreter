2a1f2ff400d066154abf50569eb0a3e17b55d244
tichymi5 <michal.tichy.y@email.cz>
2022-05-28 14:44:28 +0200

    Add repl without state

--- app/Main.hs
+++ app/Main.hs
@@ -4,8 +4,13 @@ import           System.Environment             ( getArgs )
 import           System.IO                      ( hFlush
                                                 , stdout
                                                 )
+import           Control.Monad                  ( when
+                                                , unless
+                                                )
 
-import           Evaluation                     ( execProgram )
+import           Evaluation                     ( execProgram
+                                                , Interpreter
+                                                )
 import           Parser                         ( parseProgram )
 
 read' :: IO String
@@ -19,10 +24,18 @@ eval' input = case parseProgram input of
     Left  err -> show err
     Right com -> show $ execProgram com
 
+repl :: IO ()
+repl = do
+    line <- read'
+    unless (line == ":q") $ case parseProgram line of 
+        Left err -> print err >> repl
+        Right com -> print (execProgram com) >> repl
+
 main :: IO ()
 main = do
     args <- getArgs
     case args of
+        [] -> repl
         [file] -> do
             contents <- readFile file
             putStrLn $ eval' contents
